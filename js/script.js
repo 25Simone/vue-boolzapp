6539ccbf901947f2f4d5e9e97a95aa4baa17c465
25Simone <dantsimone111@gmail.com>
2022-01-19 12:53:59 +0100

    milestone 5 completata

--- js/script.js
+++ js/script.js
@@ -10,17 +10,20 @@ new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        modalShow: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
+                        status: 'sent',
+                        modalShow: false,
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        modalShow: false,
                     }
                 ],
             },
@@ -32,17 +35,20 @@ new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        modalShow: false,
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        modalShow: false,
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        modalShow: false,
                     }
                 ],
             },
@@ -55,17 +61,20 @@ new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        modalShow: false,
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        modalShow: false,
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        modalShow: false,
                     }
                 ],
             },
@@ -77,12 +86,14 @@ new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        modalShow: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        modalShow: false,
                     }
                 ],
             },
@@ -90,6 +101,7 @@ new Vue({
         currentContact: 0,
         newMessageText: '',
         searchChat: '',
+        // inserire nel data il currentMessages per pulire codice
     },
 
     methods: {
@@ -113,7 +125,16 @@ new Vue({
                 date: '19/01/2022 10:31:31',
                 text: 'ok',
                 status: 'received',
+                modalShow: false,
             });
+        },
+        messageOptionShow: function(index){
+            const currentMessages = this.filteredContactList[this.currentContact].messages;
+            currentMessages[index].modalShow = !currentMessages[index].modalShow;
+        },
+        deleteMessage: function(index){
+            const currentMessages = this.filteredContactList[this.currentContact].messages;
+            currentMessages.splice(index, 1);
         },
     },
     // computed mi permette di richiamare le sue proprietà nell html come fossero dei data,
